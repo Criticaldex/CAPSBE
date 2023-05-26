6d23519dec5fb8ce401d39350ab666ded7b59767
gerardcoma <gerard.coma@soidemdt.com>
2023-05-26 10:52:39 +0200

    #2 Filters working

--- pages/api/contracts.ts
+++ pages/api/contracts.ts
@@ -6,34 +6,30 @@ type ResponseData = {
   message: string;
 };
 
-export default async function handler( req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { model } = req.body;
   let { fields, filter } = req.body;
-  if (req.method === 'POST' && model) {
-  try {
-    fields = (fields) ? fields.join(' ') : '';
-    const db = await mongoose.createConnection('mongodb://trial.soidemdt.com:27017/CAPFA',
-      {
-        authSource: 'admin',
-        user: 'Admin',
-        pass: 'DTSoidem.918'
-      });
 
-    const schema = mongoose.Schema;
-    const contractes2022 = await db.model(
-      model,
-      new schema(
+  if (req.method === 'POST' && model) {
+    try {
+      fields = (fields) ? fields.join(' ') : '';
+
+      const db = await mongoose.createConnection('mongodb://trial.soidemdt.com:27017/CAPFA',
         {
-          Indicador: String,
-          Resultat: [String]
-        }
-      )
-    ).find().select(fields);
-    res.status(200).json({ contractes2022 });
-  } catch (err) {
-    res.status(500).json({ ERROR: (err as Error).message });
-  }
-} else {
-  res.status(500).send({ ERROR: 'Utiliza una peticion POST para consultar este endpoint!'});
+          authSource: 'admin',
+          user: 'Admin',
+          pass: 'DTSoidem.918'
+        });
+      const schema = mongoose.Schema;
+      const data = await db.model(
+        model,
+        new schema()
+      ).find(filter).select(fields).exec();
+      res.status(200).json({ data });
+    } catch (err) {
+      res.status(500).json({ ERROR: (err as Error).message });
+    }
+  } else {
+    res.status(500).send({ ERROR: 'Utiliza una peticion POST para consultar este endpoint!' });
   }
 }
