c58010c7080a9dc355b3c9936f03e79e5bf3c1f5
gerardcoma <gerard.coma@soidemdt.com>
2023-06-21 16:42:03 +0200

    routing

--- src/app/profesionals/routing.tsx
+++ src/app/profesionals/routing.tsx
@@ -91,3 +91,60 @@ export function GetLinksSection({ sections }: any) {
       </ul>
    )
 }
+
+export function GetLinksYears({ years }: any) {
+   const pathname = usePathname();
+   const pathArray: string[] = (pathname) ? pathname.split('/') : [];
+   const view = (pathArray[2]) ? pathArray[2] : process.env.PROFESIONALS_DEFAULT_VIEW;
+   const center = (pathArray[3]) ? pathArray[3] : process.env.PROFESIONALS_DEFAULT_CENTER;
+   const section = (pathArray[4]) ? pathArray[4] : process.env.PROFESIONALS_DEFAULT_SECTION;
+
+   let links: object[] = [];
+   years.map((label: any) => (
+      links.push({
+         label: label,
+         route: `/profesionals/${view}/${center}/${section}/${label}`
+      })
+   ))
+
+   return (
+      <ul className="m-auto w-11/12 flex flex-wrap items-end justify-end my-2 text-black border-gray-900 rounded-md">
+         {links.map(({ label, route }: any) => (
+            <Link key={route} href={route}>
+               <li className="border border-blue-400 bg-blue-700 my-3 mx-4 py-2 px-5 rounded text-white hover:bg-blue-600">
+                  {label}
+               </li>
+            </Link>
+         ))}
+      </ul>
+   )
+}
+
+export function GetLinksProfesionals({ profesionals }: any) {
+   const pathname = usePathname();
+   const pathArray: string[] = (pathname) ? pathname.split('/') : [];
+   const view = (pathArray[2]) ? pathArray[2] : process.env.PROFESIONALS_DEFAULT_VIEW;
+   const center = (pathArray[3]) ? pathArray[3] : process.env.PROFESIONALS_DEFAULT_CENTER;
+   const section = (pathArray[4]) ? pathArray[4] : process.env.PROFESIONALS_DEFAULT_SECTION;
+   const year = (pathArray[5]) ? pathArray[5] : process.env.PROFESIONALS_DEFAULT_YEAR;
+
+   let links: object[] = [];
+   profesionals.map((label: any) => (
+      links.push({
+         label: label,
+         route: `/profesionals/${view}/${center}/${section}/${year}/${label.split('(').pop().split(')')[0]}`
+      })
+   ))
+
+   return (
+      <ul className="m-auto w-11/12 flex flex-wrap items-end justify-end my-2 text-black border-gray-900 rounded-md">
+         {links.map(({ label, route }: any) => (
+            <Link key={route} href={route}>
+               <li className="border border-blue-400 bg-blue-700 my-3 mx-4 py-2 px-5 rounded text-white hover:bg-blue-600">
+                  {label}
+               </li>
+            </Link>
+         ))}
+      </ul>
+   )
+}
