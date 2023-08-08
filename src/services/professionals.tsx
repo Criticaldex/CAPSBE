4de9ba4c193161c72bf2c545476c9ce47c7c6fa8
Gerard Coma <gerard.coma@soidemdt.com>
2023-08-08 13:29:57 +0200

    sin recorrer

--- src/services/professionals.tsx
+++ src/services/professionals.tsx
@@ -153,10 +153,7 @@ export const getChartIndividual = async (filtros: any, professional: string) => 
 export const getMonth = async (filtros: any) => {
    const data = await getProfessionals(filtros);
    const meses = ['Gener', 'Febrer', 'Març', 'Abril', 'Maig', 'Juny', 'Juliol', 'Agost', 'Setembre', 'Octubre', 'Novembre', 'Desembre']
-   let mes = 0;
-   for (const [key, value] of (Object.entries(data[0].professionals) as [string, any][])) {
-      if (mes < value.length - 1) mes = value.length - 1
-   }
+   let mes = data[0].professionals[Object.keys(data[0].professionals)[0]].length - 1;
 
    return meses[mes];
 }
