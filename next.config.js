35221ba8649aae09da57b5c49344c98dac2ffa17
gerardcoma <gerard.coma@soidemdt.com>
2023-09-06 15:59:56 +0200

    xx

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const nextConfig = {
       esmExternals: "loose",
       serverComponentsExternalPackages: ["mongoose", "bcryptjs"]
    },
-   distDir: 'build',
    // webpack: (config) => {
    //    config.experiments = { ...config.experiments, topLevelAwait: true };
    //    return config;
