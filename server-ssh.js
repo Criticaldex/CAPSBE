2e13b133583c70ce1e6af6943771410fe5a3f116
gerardcoma <gerard.coma@soidemdt.com>
2023-09-06 16:31:04 +0200

    pleeease

--- server-ssh.js
+++ server-ssh.js
@@ -1,33 +1,46 @@
-const { createServer: createHttpsServer } = require('https');
-const next = require('next');
-const fs = require('fs');
+const { createServer } = require('https')
+const { parse } = require('url')
+const next = require('next')
+const fs = require("fs");
 
-const prod = process.env.NODE_ENV === 'production';
-const app = next({});
-const handle = app.getRequestHandler();
-const PORT = process.env.PORT || 3000;
+const dev = process.env.NODE_ENV !== 'production'
+const hostname = 'trial.soidemdt.com'
+const port = 3333
+// when using middleware `hostname` and `port` must be provided below
+const app = next({ dev, hostname, port })
+const handle = app.getRequestHandler()
 
-if (!fs.existsSync('C:/Program Files/Splunk/etc/auth/certificados-trial')) {
-   console.error('\nError: Missing SSL certificates\n');
-   process.exit();
-}
+const httpsOptions = {
+   key: fs.readFileSync('C:/Program Files/Splunk/etc/auth/certificados-trial/privkey.pem'),
+   cert: fs.readFileSync('C:/Program Files/Splunk/etc/auth/certificados-trial/cert.pem'),
+};
 
-app
-   .prepare()
-   .then(() => {
-      const server = createHttpsServer(
-         {
-            key: fs.readFileSync('C:/Program Files/Splunk/etc/auth/certificados-trial/privkey.pem'),
-            cert: fs.readFileSync('C:/Program Files/Splunk/etc/auth/certificados-trial/cert.pem'),
-         },
-         (req, res) => handle(req, res)
-      );
+app.prepare().then(() => {
+   createServer(httpsOptions, async (req, res) => {
+      try {
+         // Be sure to pass `true` as the second argument to `url.parse`.
+         // This tells it to parse the query portion of the URL.
+         const parsedUrl = parse(req.url, true)
+         const { pathname, query } = parsedUrl
 
-      return server.listen(PORT, (err) => {
-         if (err) throw err;
-         console.log(`> Ready on https://trial.soidemdt.com:${PORT}`)
-      });
+         if (pathname === '/a') {
+            await app.render(req, res, '/a', query)
+         } else if (pathname === '/b') {
+            await app.render(req, res, '/b', query)
+         } else {
+            await handle(req, res, parsedUrl)
+         }
+      } catch (err) {
+         console.error('Error occurred handling', req.url, err)
+         res.statusCode = 500
+         res.end('internal server error')
+      }
    })
-   .catch((err) => {
-      console.error(err);
-   });
+      .once('error', (err) => {
+         console.error(err)
+         process.exit(1)
+      })
+      .listen(port, () => {
+         console.log(`> Ready on http://${hostname}:${port}`)
+      })
+})
