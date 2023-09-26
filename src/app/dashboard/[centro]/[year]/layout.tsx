5b3afb0433a155d5bf81bb6397bdb747cf0a7ba0
gerardcoma <gerard.coma@soidemdt.com>
2023-09-26 16:07:32 +0200

    no DMA per centelles Calls nomes per Capsbe

--- src/app/dashboard/[centro]/[year]/layout.tsx
+++ src/app/dashboard/[centro]/[year]/layout.tsx
@@ -7,6 +7,7 @@ import { getDmaAssignada, getDmaDashboard, getRegressioLineal } from "@/services
 import { Dma } from "./dma";
 import { getCallsToday } from "@/services/calls";
 import { CallsTable } from "./callsTable";
+import { getSession } from "@/services/session"
 
 export default async function LayoutDashboard({ children, params }: any) {
    const { year, centro } = params;
@@ -19,15 +20,19 @@ export default async function LayoutDashboard({ children, params }: any) {
          nameCentro = center.name
       }
    })
-
+   const session = await getSession();
    const calls = await getCallsToday();
    const eqas = await getEqasContracts(year, centros);
    const iqf = await getIqfDashboard(up);
    const basal = await getBasal(up);
-
-   const dma = await getDmaDashboard(up);
-   const dma_assignada = await getDmaAssignada(up);
-   const dma_regressio_lineal = await getRegressioLineal(up, dma);
+   let dma = null;
+   let dma_assignada = null;
+   let dma_regressio_lineal = null;
+   if (session?.user.db != 'Centelles') {
+      dma = await getDmaDashboard(up);
+      dma_assignada = await getDmaAssignada(up);
+      dma_regressio_lineal = await getRegressioLineal(up, dma);
+   }
 
    return (
       <article className="min-h-fit">
@@ -44,12 +49,14 @@ export default async function LayoutDashboard({ children, params }: any) {
          </section>
          <div className="flex flex-row justify-between mx-2 mb-2">
             <div className="w-1/2 p-1 mr-1 bg-bgLight rounded-md shadow-xl">
-               <Dma
-                  name={`DMA ${nameCentro}`}
-                  data={dma}
-                  objectiu={dma_assignada}
-                  regresion={dma_regressio_lineal}
-               />
+               {session?.user.db != 'Centelles' &&
+                  <Dma
+                     name={`DMA ${nameCentro}`}
+                     data={dma}
+                     objectiu={dma_assignada}
+                     regresion={dma_regressio_lineal}
+                  />
+               }
             </div>
             <div className="w-1/2 p-1 ml-1 bg-bgLight rounded-md shadow-xl">
                <Iqf
@@ -59,14 +66,16 @@ export default async function LayoutDashboard({ children, params }: any) {
                />
             </div>
          </div>
-         <div className="flex flex-row justify-between mx-2 mb-2">
-            <div className="w-screen p-1 bg-bgLight rounded-md shadow-xl">
-               <CallsTable
-                  data={calls}
-                  centros={centros}
-               />
+         {session?.user.db == 'Capsbe' &&
+            <div className="flex flex-row justify-between mx-2 mb-2">
+               <div className="w-screen p-1 bg-bgLight rounded-md shadow-xl">
+                  <CallsTable
+                     data={calls}
+                     centros={centros}
+                  />
+               </div>
             </div>
-         </div>
+         }
       </article>
    );
 }
