911f256e329a43e5e8a1e525fbc0897c0f2c7190
gerardcoma <gerard.coma@soidemdt.com>
2023-10-05 15:04:30 +0200

    Contracts Patch method & Schema

--- src/app/api/contracts/route.ts
+++ src/app/api/contracts/route.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 import dbConnect from '@/lib/dbConnect'
-import contractSchema from '@/schemas/contract'
+import contractSchema, { ContractsIface } from '@/schemas/contract'
 import { NextResponse } from 'next/server'
 
 export async function POST(request: Request) {
@@ -15,6 +15,35 @@ export async function POST(request: Request) {
       }
       const contract: any = await db.models.contract.find(body.filter).select(fields).lean();
       return NextResponse.json(contract);
+   } catch (err) {
+      return NextResponse.json({ ERROR: (err as Error).message });
+   }
+}
+
+export async function PATCH(request: Request) {
+   try {
+      const body: ContractsIface = await request.json();
+      if (!body.identificador || !body.any || !body.centre) {
+         return NextResponse.json(`identificador, any i centre obligatoris!`);
+      }
+      const filter = {
+         any: body.any,
+         centre: body.centre,
+         identificador: body.identificador
+      }
+
+      const { dbName, ...bodyWithoutDB } = body
+      await dbConnect();
+      const db = mongoose.connection.useDb(dbName, { useCache: true });
+      if (!db.models.contract) {
+         db.model('contract', contractSchema);
+      }
+      const res = await db.models.contract.findOneAndUpdate(filter, bodyWithoutDB, {
+         new: true,
+         upsert: false,
+         rawResult: true
+      }).lean();
+      return NextResponse.json(res);
    } catch (err) {
       return NextResponse.json({ ERROR: (err as Error).message });
    }
--- src/schemas/contract.ts
+++ src/schemas/contract.ts
@@ -1,5 +1,38 @@
 import mongoose from 'mongoose'
 
-const Schema = new mongoose.Schema({});
+export interface ContractsIface {
+   identificador: string,
+   indicador: string,
+   any: string,
+   centre: string,
+   invers: string,
+   objectiu: string,
+   dbName: string
+}
+
+const Schema = new mongoose.Schema({
+   identificador: {
+      type: String,
+      required: [true, 'L\'identificador es obligatori!']
+   },
+   indicador: {
+      type: String,
+      required: [true, 'L\'indicador es obligatori!']
+   },
+   any: {
+      type: String,
+      required: [true, 'L\'Any es obligatori!']
+   },
+   centre: {
+      type: String,
+      required: [true, 'El Centre es obligatori!']
+   },
+   invers: {
+      type: Boolean,
+   },
+   objectiu: {
+      type: Number,
+   },
+});
 
 export default Schema
