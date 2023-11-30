53f98b4d0cc6f785b44b98e344fcc9d2b98789f4
gerardcoma <gerard.coma@soidemdt.com>
2023-11-30 15:14:50 +0100

    ident

--- src/services/call_intervals.tsx
+++ src/services/call_intervals.tsx
@@ -51,15 +51,13 @@ export const getHoursChart = async (year: string, month: string, day: string, ce
       color: "var(--red)",
       data: [],
       yAxis: 0
-   },
-   {
+   }, {
       type: 'spline',
       name: 'Operadors',
       color: "var(--lightBlue)",
       data: [],
       yAxis: 1
-   },
-   {
+   }, {
       type: 'spline',
       name: 'Cua',
       color: "var(--orange)",
