14564c43a0edae8121bfe40578a2add92cc4737c
gerardcoma <gerard.coma@soidemdt.com>
2024-01-23 17:15:03 +0100

    working routes for professional need to do frontend

--- src/app/professionals/routing.tsx
+++ src/app/professionals/routing.tsx
@@ -1,7 +1,7 @@
 'use client'
 import Link from "next/link"
 import { usePathname, useRouter } from 'next/navigation';
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export function GetLinksView() {
    const pathname = usePathname();
@@ -71,6 +71,50 @@ export function GetLinksCentro({ centros }: any) {
 }
 
 export function GetLinksSection({ sections }: any) {
+   const pathname = usePathname();
+   const router = useRouter();
+
+   const pathArray: string[] = (pathname) ? pathname.split('/') : [];
+   const view = (pathArray[2]) ? pathArray[2] : process.env.PROFESSIONALS_DEFAULT_VIEW;
+   const center = (pathArray[3]) ? pathArray[3] : process.env.DEFAULT_CENTER;
+   const sect = (pathArray[4]) ? pathArray[4] : sections[0].replaceAll(' ', '_');
+
+   let links: object[] = [];
+   sections.map((label: any) => (
+      links.push({
+         label: label,
+         route: `/professionals/${view}/${center}/${label.replaceAll(' ', '_')}`
+      })
+   ))
+
+   useEffect(() => {
+      let coincidencia = links.filter((link: any) => pathname?.includes(link.route))
+      console.log('coincidencia: ', coincidencia);
+      if (coincidencia.length == 0)
+         router.push(`/professionals/${view}/${center}/${sections[0].replaceAll(' ', '_')}`)
+   }, []);
+
+   return (
+      <>
+         <label>
+            <select value={`/professionals/${view}/${center}/${sect}`}
+               className={'mx-2 py-2 px-5 rounded-md text-textColor font-bold border border-darkBlue bg-bgDark hover:bg-bgLight'}
+               onChange={e => {
+                  router.push(e.target.value)
+               }}>
+
+               {sections.map((section: any) => {
+                  return <option key={section} value={`/professionals/${view}/${center}/${section.replaceAll(' ', '_')}`}>
+                     {section}
+                  </option>
+               })}
+            </select>
+         </label>
+      </>
+   )
+}
+
+export function GetLinksSection_OLD({ sections }: any) {
    const pathname = usePathname();
    const pathArray: string[] = (pathname) ? pathname.split('/') : [];
    const view = (pathArray[2]) ? pathArray[2] : process.env.PROFESSIONALS_DEFAULT_VIEW;
