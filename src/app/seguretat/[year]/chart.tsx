c01c5b414956151a522e7a88983230976ed104a9
gerardcoma <gerard.coma@soidemdt.com>
2024-01-30 17:21:23 +0100

    done Seguretat

---
+++ src/app/seguretat/[year]/chart.tsx
@@ -0,0 +1,82 @@
+'use client'
+import Highcharts from 'highcharts'
+import HighchartsExporting from 'highcharts/modules/exporting'
+import HighchartsExportData from 'highcharts/modules/export-data'
+import HighchartsReact from 'highcharts-react-official'
+import { chartOptions } from '@/components/chart.components'
+import { useEffect } from 'react'
+
+if (typeof Highcharts === "object") {
+   HighchartsExporting(Highcharts)
+   HighchartsExportData(Highcharts)
+}
+
+export function Chart({ name, data, objectiu, categories, setter }: any) {
+
+   let centrosClass = ['centrosGeriatria', 'centrosSnc']
+   useEffect(() => {
+      centrosClass.forEach(clase => {
+         let contenedor = document.getElementsByClassName(clase)
+         for (var i = 0; i < contenedor.length; i++) {
+            let p = contenedor[i].getElementsByTagName('p')
+            p[0].style.borderColor = `var(--highcharts${i})`
+         };
+      })
+   }, [])
+
+   let max = 0;
+   data.forEach((elem: any) => {
+      elem.data.map((i: any) => {
+         max = (i > max) ? i : max;
+      });
+   });
+   max = (objectiu > max) ? objectiu : max;
+   const options = {
+      ...chartOptions,
+      chart: {
+         type: 'column',
+         spacingTop: 30
+      },
+      title: {
+         text: name
+      },
+      series: data,
+      yAxis: {
+         ...chartOptions.yAxis,
+         max: max
+      },
+      xAxis: {
+         categories: categories,
+      },
+      legend: {
+         enabled: false,
+      },
+      tooltip: {
+         pointFormat: '{series.name}: <b>{point.y} punts</b><br/>'
+      },
+      plotOptions: {
+         series: {
+            ...chartOptions.plotOptions.series,
+            dataLabels: {
+               enabled: true,
+               style: {
+                  textOutline: 'none'
+               }
+            },
+            events: {
+               click: function (event: any) {
+                  setter(event.point.category);
+               }
+            }
+         },
+      }
+   }
+
+   return (
+      <HighchartsReact
+         highcharts={Highcharts}
+         options={options}
+      />
+   )
+}
+
---
+++ src/app/seguretat/layout.tsx
@@ -0,0 +1,28 @@
+import { GetLinksYears } from "./routing";
+import { getCenters } from "@/services/centros";
+import { getYears } from "@/services/seguretats";
+
+export default async function ContractsLayout({ children }: any) {
+   const centros = await getCenters();
+   const years = await getYears(centros);
+
+   return (
+      <div>
+         <title>Seguretat</title>
+         <div className="mt-2 bg-light text-right flex justify-between items-center">
+            <div className="flex justify-between grow mb-2 mx-2">
+               <GetLinksYears
+                  years={years}
+               />
+            </div>
+            <div className="bg-light text-right flex justify-end items-center">
+               <h1 className="right-0 w-auto mx-10 font-semibold text-2xl italic">{"Seguretat en l'ús de Medicaments EAP"}</h1>
+            </div>
+         </div>
+         <hr className="w-11/12 m-auto mt-0 border-t-2 border-darkBlue" />
+         <main className="m-2">
+            {children}
+         </main>
+      </div>
+   )
+}
