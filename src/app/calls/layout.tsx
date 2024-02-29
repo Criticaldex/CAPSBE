8173f33ad559a4e9a2ff21c85f681dfaaea1ea38
gerardcoma <gerard.coma@soidemdt.com>
2024-02-29 14:50:50 +0100

    titol trucades

--- src/app/calls/layout.tsx
+++ src/app/calls/layout.tsx
@@ -23,7 +23,7 @@ export default async function ContractsLayout({ children }: any) {
                {/* links */}
             </div>
             <div className="bg-light text-right flex justify-end items-center">
-               <h1 className="right-0 w-auto mx-10 font-semibold text-2xl italic">Quadre Comandament</h1>
+               <h1 className="right-0 w-auto mx-10 font-semibold text-2xl italic">Control Trucades</h1>
             </div>
          </div>
          <hr className="w-11/12 m-auto border-b border-darkBlue" />
