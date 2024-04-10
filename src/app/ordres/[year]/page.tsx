853eca6b7a6ea3195e5078010193a7f84b1cd718
gerardcoma <gerard.coma@soidemdt.com>
2024-04-10 08:36:33 +0200

    fix position ordres

--- src/app/ordres/[year]/page.tsx
+++ src/app/ordres/[year]/page.tsx
@@ -6,13 +6,12 @@ export default async function OrdresYear({ params }: { params: any }) {
    const { year } = params
    const centros = await getCenters();
 
-
    const filter = { "any": year };
    const indicadores = await getOrdresIndicators(filter);
 
    return (
-      <section className="w-full h-screen m-auto flex justify-center mt-10 p-2">
-         <article className="w-full h-3/4 overflow-auto rounded-lg scrollable">
+      <section className="w-full h-screen m-auto flex justify-center p-2">
+         <article className="w-full pb-4 h-fit overflow-auto rounded-lg scrollable">
             <OrdresTable
                data={indicadores}
                centros={centros}
